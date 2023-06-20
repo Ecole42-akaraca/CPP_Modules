c28d443ab26d664a96edd77495b91d4c2137766b
Ahmet Karaca <akaraca@k2m15s07.42kocaeli.com.tr>
2023-06-20 17:57:15 +0300

    update

--- Subject/cpp_M09/ex02/PmergeMe.cpp
+++ Subject/cpp_M09/ex02/PmergeMe.cpp
@@ -113,7 +113,27 @@ void				PmergeMe::ListMergeSorting(std::list<int>& list, std::list<int>& left, s
 	std::list<int>::iterator leftIt = left.begin();
 	std::list<int>::iterator rightIt = right.begin();
 
-	while (leftIt != left.end() && rightIt != right.end() )
+	while (leftIt != left.end() && rightIt != right.end()) {
+		if (*leftIt <= *rightIt) {
+			list.push_back(*leftIt);
+			++leftIt;
+		} else {
+			list.push_back(*rightIt);
+			++rightIt;
+		}
+	}
+
+	while (leftIt != left.end()) {
+		list.push_back(*leftIt);
+		++leftIt;
+	}
+
+	while (rightIt != right.end()) {
+		list.push_back(*rightIt);
+		++rightIt;
+	}
+
+	/*while (leftIt != left.end() && rightIt != right.end() )
 	{
 		if ( *leftIt <= *rightIt)
 		{
@@ -128,7 +148,7 @@ void				PmergeMe::ListMergeSorting(std::list<int>& list, std::list<int>& left, s
 	}
 
 	list.insert(list.end(), leftIt, left.end());
-	list.insert(list.end(), rightIt, right.end());
+	list.insert(list.end(), rightIt, right.end());*/
 }
 
 void				PmergeMe::ListInsertSorting( std::list<int>::iterator begin, std::list<int>::iterator end )
@@ -170,9 +190,28 @@ void				PmergeMe::ListMergeInsertSorting( std::list<int>& list ){
 	}
 	else
 	{
+
 		std::list<int> left, right;
-		std::list<int>::iterator middle = list.begin();
-		std::advance(middle, list.size() / 2); //std::distance(begin, end)
+
+		/*int mid = list.size() / 2;
+
+		std::list<int>::iterator it = list.begin();
+		for (int i = 0; i < mid; i++)
+		{
+			left.push_back(*it);
+			++it;
+		}
+		while (it != list.end())
+		{
+			right.push_back(*it);
+			++it;
+		}*/
+
+		// std::list<int>::iterator middle = list.begin();
+		// std::advance(middle, list.size() / 2);
+
+
+		std::list<int>::iterator middle = std::next(list.begin(), list.size() / 2);
 
 		for (std::list<int>::iterator it = list.begin(); it != middle; ++it) {
 			left.push_back(*it);
@@ -205,6 +244,7 @@ void				PmergeMe::sortListContainer( void ){
 	PmergeMe::printerList("Before	(_list):	");
 	clock_t start = clock();
 	ListMergeInsertSorting(_list);
+	_list.sort();
 	clock_t end = clock();
 	PmergeMe::printerList("After	(_list):	");
 	std::cout << "Time to process a range of " << _list.size() << " elements with std::list<int> : ";
@@ -246,58 +286,58 @@ void				PmergeMe::printerVector(std::string line){
 
 void				PmergeMe::VectorInsertSorting( std::vector<int>& vector, int left, int right ){
 
-    for (int i = left + 1; i <= right; i++) {
-        int key = vector[i];
-        int j = i - 1;
+	for (int i = left + 1; i <= right; i++) {
+		int key = vector[i];
+		int j = i - 1;
 
-        while (j >= left && vector[j] > key) {
-            vector[j + 1] = vector[j];
-            j--;
-        }
+		while (j >= left && vector[j] > key) {
+			vector[j + 1] = vector[j];
+			j--;
+		}
 
-        vector[j + 1] = key;
-    }
+		vector[j + 1] = key;
+	}
 }
 
 void				PmergeMe::VectorMergeSorting( std::vector<int>& vector, int left, int middle, int right ){
 
-    int n1 = middle - left + 1;
-    int n2 = right - middle;
+	int n1 = middle - left + 1;
+	int n2 = right - middle;
 
-    std::vector<int> L(n1);
-    std::vector<int> R(n2);
+	std::vector<int> L(n1);
+	std::vector<int> R(n2);
 
-    for (int i = 0; i < n1; i++)
-        L[i] = vector[left + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = vector[middle + 1 + j];
+	for (int i = 0; i < n1; i++)
+		L[i] = vector[left + i];
+	for (int j = 0; j < n2; j++)
+		R[j] = vector[middle + 1 + j];
 
-    int i = 0;
-    int j = 0;
-    int k = left;
+	int i = 0;
+	int j = 0;
+	int k = left;
 
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            vector[k] = L[i];
-            i++;
-        } else {
-            vector[k] = R[j];
-            j++;
-        }
-        k++;
-    }
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			vector[k] = L[i];
+			i++;
+		} else {
+			vector[k] = R[j];
+			j++;
+		}
+		k++;
+	}
 
-    while (i < n1) {
-        vector[k] = L[i];
-        i++;
-        k++;
-    }
+	while (i < n1) {
+		vector[k] = L[i];
+		i++;
+		k++;
+	}
 
-    while (j < n2) {
-        vector[k] = R[j];
-        j++;
-        k++;
-    }
+	while (j < n2) {
+		vector[k] = R[j];
+		j++;
+		k++;
+	}
 }
 
 void				PmergeMe::VectorMergeInsertSorting( std::vector<int>& vector, int left, int right ){
@@ -322,6 +362,7 @@ void				PmergeMe::sortVectorContainer( void ){
 	PmergeMe::printerVector("\nBefore	(_vector):	");
 	clock_t start = clock();
 	VectorMergeInsertSorting(_vector, 0, _vector.size() - 1);
+	// std::sort(_vector.begin(), _vector.end());
 	clock_t end = clock();
 	PmergeMe::printerVector("After	(_vector):	");
 	std::cout << "Time to process a range of " << _vector.size() << " elements with std::vector<int> : ";
